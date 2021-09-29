4a4c8b591ad3b05550d43b3a264f2b5c6afa7c0e
Igor <igormgg@hotmail.com>
2021-09-29 19:39:02 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Gestão de transações</header>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
